e6a91528b808695c2747e736272fef57c96c2baa
iceshoesss <iceshoesss@gmail.com>
2022-06-04 22:26:51 +0800

    添加注释

--- TCPclient.gd
+++ TCPclient.gd
@@ -1,61 +1,37 @@
 class_name TCP_connect
 extends Node
 
+# 1.新建TCP类
 onready var client = StreamPeerTCP.new()
-var recmsg
-
-func _ready():
-	pass
-#	client.connect_to_host('192.168.31.182',8095)
-#	client.set_stream_peer(1)
-
-#func _process(delta):
-#	if client.get_status() == client.STATUS_CONNECTED:
-#		if client.get_available_bytes() >=3:
-#			print(client.get_available_bytes())
-#			print(client.get_utf8_string())
 
 
 func _on_Button_pressed():
 	var msg = $Control/TextEdit.text
+	# 发送数据，将其转码为utf8，数据必须以bytes类型传输
 	client.put_data(msg.to_utf8())
-	print(typeof(msg.to_utf8()))
-
-#	var msgrcv = client.get_data(1024)
-#	print(msgrcv)
 
 
 func _on_Button2_pressed():
+	# 2.连接服务器，其中参数ip为str，端口为int
 	client.connect_to_host('192.168.31.182',8095)
-func create_server():
-	pass
-
-func create_client():
-	pass
 
 
 func _on_State_pressed():
+	# 查询连接状态
 	print(client.get_status())
 
 
 func _on_Disconnect_pressed():
+	# 端开连接
 	client.disconnect_from_host()
 
 
 
 func _on_Getdata_pressed():
-#	recmsg = client.get_data(6)[1]
-#	var a = PoolByteArray([79, 75, 97, 98, 115, 100])
-#	print(recmsg)
-#	print(bytes2var(a))
+	# 3.接收数据
+	# rev_num获得数据的字节长度
 	var rev_num = client.get_available_bytes()
-	print(rev_num)
+#	print(rev_num)
+	# 将byte转为string并且打印出来
 	var data = client.get_utf8_string(rev_num)
 	print(data)
-
-
-#		rev_num = client.get_available_bytes()
-#		if rev_num > 0:
-#			var data = client.get_utf8_string(rev_num)
-#			print(data)
-#			continue
